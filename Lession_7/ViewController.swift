08e64979947e187afc077daa3060ba592695375c
NG BAO <nguyen.van.bao@idevbaos-macbook-pro.local>
2018-05-11 15:42:40 +0700

    Initial Commit

---
+++ Lession_7/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lession_7
+//
+//  Created by nguyen.van.bao on 11/05/2018.
+//  Copyright © 2018 nguyen.van.bao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
